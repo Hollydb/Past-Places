07ab4405cce751a7d97a9120e2c245c5fa5af452
Daniel Hongel and Holly DeBelser <holly@>
2019-06-17 11:57:06 -0700

    Add buisness logic for locationBook and prototype function add Location

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,13 @@
+//Business Logic for LocationBook -------
+
+function LocationBook() {
+  this.locations = []
+}
+
+LocationBook.prototype.addLocation = function(location) {
+  this.locations.push(location);
+}
+//Business Logic for Locations --------
 function Location(country, city, year) {
   this.country = country;
   this.city = city;

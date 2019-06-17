273122429d079b8a09b6a373a694870f7edc4a7e
Daniel Hongel and Holly DeBelser <holly@>
2019-06-17 15:06:44 -0700

    remove the EVIL semicolon of DOOM.

--- js/scripts.js
+++ js/scripts.js
@@ -4,6 +4,21 @@ function LocationBook() {
   this.locations = []
 }
 
+LocationBook.prototype.findLocation = function(country) {
+  console.log(this)
+  for (var i = 0; i < this.locations.length; i ++) {
+    console.log("first" + i);
+    // if (this.locations[i]) {
+    //   console.log("inside" + i);
+      if (this.locations[i].country == country) {
+              console.log("insider" + i);
+        return this.locations[i];
+      }
+    // }
+  };
+  return false;
+}
+
 LocationBook.prototype.addLocation = function(location) {
   this.locations.push(location);
 }
@@ -17,3 +32,10 @@ function Location(country, city, year) {
 Location.prototype.cityCountry = function() {
   return this.city + ", " + this.country;
 }
+
+//Temporary code for adding some locations.
+var locationbook = new LocationBook();
+var place = new Location ("Costa Rica", ["Tamarindo", "Playas del Coco"], 2006);
+var place2 = new Location ("Japan", "Tokyo", 2018);
+locationbook.addLocation(place);
+locationbook.addLocation(place2);
